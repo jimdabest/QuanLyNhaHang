e1d50145d08547bcab35ecdbe4e6512e9214938b
PhamNguyen <jim.phamnguyen@gmail.com>
2026-03-25 11:46:51 +0700

    Update file KhachHangDAO

--- src/quanlynhahang/dao/KhachHangDAO.java
+++ src/quanlynhahang/dao/KhachHangDAO.java
@@ -1,4 +1,36 @@
 package quanlynhahang.dao;
 
-public class KhachHangDAO {
+import quanlynhahang.dto.KhachHangDTO;
+
+import java.util.ArrayList;
+
+public class KhachHangDAO implements IDAO<KhachHangDTO, String>{
+    public KhachHangDAO() {
+        super();
+    }
+
+    @Override
+    public boolean insert(KhachHangDTO obj) {
+        return false;
+    }
+
+    @Override
+    public boolean update(KhachHangDTO obj) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String key) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<KhachHangDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public KhachHangDTO getById(String key) {
+        return null;
+    }
 }
